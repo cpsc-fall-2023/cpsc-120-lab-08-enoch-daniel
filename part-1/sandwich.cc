552a9063eb75fd7616755579f8738b0f77077742
Goggles41 <143146758+Goggles41@users.noreply.github.com>
2023-10-25 01:07:44 +0000

    sandwich.cc

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -14,11 +14,11 @@ int main(int argc, char* argv[]) {
     std::cout << "Error: You must supply three arguments";
     return 1;
   }
-  
+
   std::string protein = {arguments.at(1)};
   std::string bread = {arguments.at(2)};
   std::string condiment = {arguments.at(3)};
- 
+
   std::cout << "Your order: A " << protein << " sandwich on " << bread
             << " with " << condiment;
   return 0;
